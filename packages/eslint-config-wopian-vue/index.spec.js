7cae71afe1eabc8d23b187f679588d057a16d3ed
James Harris <wopian@wopian.me>
2018-02-07 01:00:37 +0000

    test: add tests

---
+++ packages/eslint-config-wopian-vue/index.spec.js
@@ -0,0 +1,22 @@
+const { CLIEngine } = require('eslint')
+const config = require('./')
+
+const cli = new CLIEngine({
+  useEslintrc: false,
+  configFile: `${__dirname}/.eslintrc.json`
+})
+
+describe('wopian-react', () => {
+  it('should be a valid ESLint config', () => {
+    const code = 'var foo = 1\nvar bar = function () {}\nbar(foo)\n'
+    expect(cli.executeOnText(code).errorCount).toBe(0)
+  })
+
+  it('should extend wopian', () => {
+    expect(config.extends).toBe('wopian')
+  })
+
+  it('should use html', () => {
+    expect(config.plugins).toEqual(['html'])
+  })
+})
---
+++ packages/eslint-config-wopian/index.spec.js
@@ -0,0 +1,27 @@
+const { CLIEngine } = require('eslint')
+const config = require('./')
+
+const cli = new CLIEngine({
+  useEslintrc: false,
+  configFile: `${__dirname}/.eslintrc.json`
+})
+
+describe('wopian', () => {
+  it('should be a valid ESLint config', () => {
+    const code = 'var foo = 1\nvar bar = function () {}\nbar(foo)\n'
+    expect(cli.executeOnText(code).errorCount).toBe(0)
+  })
+
+  it('should extend standard', () => {
+    expect(config.extends).toBe('standard')
+  })
+
+  it('should use babel to parse files', () => {
+    expect(config.parser).toBe('babel-eslint')
+  })
+
+  it('should use jest', () => {
+    expect(config.plugins).toEqual(['jest'])
+    expect(config.env.jest).toBe(true)
+  })
+})
